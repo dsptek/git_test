e7cc07bb0a6ee0382478fcf0d61c8373afbfc1d2
dsptek <jhrzone@dsptek.co.kr>
2017-09-19 17:26:41 +0900

    @RequestMapping("/user")

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -1,6 +1,18 @@
-package net.slipp.web;
+package net.slipp.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+// db랑 연결되는거라는 알려 주기 위해서 @Entity 애노테이션을 사용한다. 
+@Entity
 public class User {
+	@Id
+	@GeneratedValue // 자동으로 증가 시켜 준다.
+	private Long id;
+	
+	@Column(nullable=false, length=20)
 	private String userId;
 	private String password;
 	private String name;
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,44 +1,33 @@
 package net.slipp.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 	
-//	@GetMapping("/create")
-//	private String create(String userId, String password, String name, String email) {
-//		
-//		System.out.println("userId : "+ userId + ", password : " + password);
-//		return "index";
-//	}
-	
-//	@PostMapping("/create")
-//	private String create(String userId, String password, String name, String email) {
-//		
-//		System.out.println("userId : "+ userId + ", password : " + password);
-//		return "index";
-//	}
-	
-	private List<User> users = new ArrayList<User>(); 
+	@Autowired
+	private UserRepository userRepository;
 	
 	@PostMapping("/create")
 	private String create(User user) {
 		System.out.println("user : "+ user);
-		users.add(user);
-		return "redirect:/list";
+		userRepository.save(user);
+		return "redirect:/user/list";
 	}
 	
 	
-	@GetMapping("/list")
+	@GetMapping("/list") 
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
 	}
-
 }
